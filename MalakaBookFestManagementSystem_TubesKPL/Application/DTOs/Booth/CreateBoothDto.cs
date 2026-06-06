3d0601b36fefd1e6b88c24445c850fa21556a4fa
Vastuolu <naufalnabillansyah49@gmail.com>
2026-06-06 17:54:24 +0700

    feat: add DTO validation

--- MalakaBookFestManagementSystem_TubesKPL/Application/DTOs/Booth/CreateBoothDto.cs
+++ MalakaBookFestManagementSystem_TubesKPL/Application/DTOs/Booth/CreateBoothDto.cs
@@ -1,11 +1,20 @@
 using MalakaBookFest.Core.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace MalakaBookFest.Application.DTOs.Booth;
 
 public class CreateBoothDto
 {
+    [Required]
+    [StringLength(150, MinimumLength = 2)]
     public string BoothName { get; set; } = string.Empty;
+
+    [StringLength(1000)]
     public string? Description { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string BoothNumber { get; set; } = string.Empty;
+
     public BoothCategory Category { get; set; } = BoothCategory.Other;
 }
--- MalakaBookFestManagementSystem_TubesKPL/Application/DTOs/Booth/UpdateBoothDto.cs
+++ MalakaBookFestManagementSystem_TubesKPL/Application/DTOs/Booth/UpdateBoothDto.cs
@@ -1,11 +1,17 @@
 using MalakaBookFest.Core.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace MalakaBookFest.Application.DTOs.Booth;
 
 public class UpdateBoothDto
 {
+    [StringLength(150, MinimumLength = 2)]
     public string? BoothName { get; set; }
+
+    [StringLength(1000)]
     public string? Description { get; set; }
+
     public BoothCategory? Category { get; set; }
+
     public bool? IsActive { get; set; }
 }
